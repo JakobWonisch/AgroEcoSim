90768574465136d8fac913925b2bfb7f3dd402ac
JakobWonisch <jakob.wonisch@gmail.com>
2026-07-05 19:02:00 +0200

    fix height of species tab

--- ThreeFrontend/src/components/app.tsx
+++ ThreeFrontend/src/components/app.tsx
@@ -1,93 +1,185 @@
-import { signal } from '@preact/signals';
-import { Fragment, h } from 'preact';
-import { DownloadRoots } from './hud/DownloadRoots';
-import { ExactPreview } from './hud/ExactPreview';
-import { ExportImport } from './hud/ExportImport';
-import { FieldSizeD } from './hud/FieldCellsD';
-import { FieldSizeX } from './hud/FieldCellsX';
-import { FieldSizeZ } from './hud/FieldCellsZ';
-import { FieldItemRegex } from './hud/FieldItemRegex';
-import { FieldModelUpload } from './hud/FieldModelUpload';
-import { FieldResolution } from './hud/FieldResolution';
-import { HoursPerTick } from './hud/HoursPerTick';
-import { InitNumber } from './hud/InitNumber';
-import { Obstacles } from './hud/Obstacles';
-import { ProgressBar } from './hud/ProgressBar';
-import { Randomize } from './hud/Randomize';
-import { Renderer } from './hud/RenderMode';
-import { SamplesPerPixel } from './hud/SamplesPerPixel';
-import { Seeds } from './hud/Seeds';
-import { SpeciesList } from './hud/Species';
-import { Start } from './hud/Start';
-import { TotalHours } from './hud/TotalHours';
-import { VisualMapping } from './hud/VisualMapping';
-import { PickedDetails } from './viewport/PickedDetails';
-import { TerrainsList } from './viewport/TerrainsList';
-import ThreeSceneFn from './viewport/ThreeSceneFn';
+import { signal } from "@preact/signals";
+import { Fragment, h } from "preact";
+import { DownloadRoots } from "./hud/DownloadRoots";
+import { ExactPreview } from "./hud/ExactPreview";
+import { ExportImport } from "./hud/ExportImport";
+import { FieldSizeD } from "./hud/FieldCellsD";
+import { FieldSizeX } from "./hud/FieldCellsX";
+import { FieldSizeZ } from "./hud/FieldCellsZ";
+import { FieldItemRegex } from "./hud/FieldItemRegex";
+import { FieldModelUpload } from "./hud/FieldModelUpload";
+import { FieldResolution } from "./hud/FieldResolution";
+import { HoursPerTick } from "./hud/HoursPerTick";
+import { InitNumber } from "./hud/InitNumber";
+import { Obstacles } from "./hud/Obstacles";
+import { ProgressBar } from "./hud/ProgressBar";
+import { Randomize } from "./hud/Randomize";
+import { Renderer } from "./hud/RenderMode";
+import { SamplesPerPixel } from "./hud/SamplesPerPixel";
+import { Seeds } from "./hud/Seeds";
+import { SpeciesList } from "./hud/Species";
+import { Start } from "./hud/Start";
+import { TotalHours } from "./hud/TotalHours";
+import { VisualMapping } from "./hud/VisualMapping";
+import { PickedDetails } from "./viewport/PickedDetails";
+import { TerrainsList } from "./viewport/TerrainsList";
+import ThreeSceneFn from "./viewport/ThreeSceneFn";
 //import {Tab, initTE } from "tw-elements"; initTE({ Tab }); //tried but failed
 
 const tabs = signal("tab-home");
 
 const App = () => {
-	return(
-	<>
-		<main id="app">
-			<ThreeSceneFn />
-		</main>
-		<nav id="hud" className={tabs.value.endsWith("tab-spec") ? 'expanded' : ''}>
-			<ul role="tablist">
-				<li role="presentation"><a role="tab" onClick={e => tabs.value = "tab-home"} aria-selected={tabs.value.endsWith("tab-home")}>Home</a></li>
-				<li role="presentation"><a role="tab" onClick={e => tabs.value = "tab-sim"} aria-selected={tabs.value.endsWith("tab-sim")}>Simulation</a></li>
-				<li role="presentation"><a role="tab" onClick={e => tabs.value = "tab-terrain"} aria-selected={tabs.value.endsWith("tab-terrain")}>Terrain</a></li>
-				<li role="presentation"><a role="tab" onClick={e => tabs.value = "tab-spec"} aria-selected={tabs.value.endsWith("tab-spec")}>Species</a></li>
-				<li role="presentation"><a role="tab" onClick={e => tabs.value = "tab-plants"} aria-selected={tabs.value.endsWith("tab-plants")}>Plants</a></li>
-				<li role="presentation"><a role="tab" onClick={e => tabs.value = "tab-obstacles"} aria-selected={tabs.value.endsWith("tab-obstacles")}>Obstacles</a></li>
-				<li role="presentation"><a role="tab" onClick={e => tabs.value = "tab-analysis"} aria-selected={tabs.value.endsWith("tab-analysis")}>Analysis</a></li>
-				{() => "TODO: make analysis a separate panel always visible"}
-			</ul>
+  return (
+    <>
+      <main id="app">
+        <ThreeSceneFn />
+      </main>
+      <nav
+        id="hud"
+        className={tabs.value.endsWith("tab-spec") ? "expanded" : ""}
+      >
+        <ul role="tablist">
+          <li role="presentation">
+            <a
+              role="tab"
+              onClick={(e) => (tabs.value = "tab-home")}
+              aria-selected={tabs.value.endsWith("tab-home")}
+            >
+              Home
+            </a>
+          </li>
+          <li role="presentation">
+            <a
+              role="tab"
+              onClick={(e) => (tabs.value = "tab-sim")}
+              aria-selected={tabs.value.endsWith("tab-sim")}
+            >
+              Simulation
+            </a>
+          </li>
+          <li role="presentation">
+            <a
+              role="tab"
+              onClick={(e) => (tabs.value = "tab-terrain")}
+              aria-selected={tabs.value.endsWith("tab-terrain")}
+            >
+              Terrain
+            </a>
+          </li>
+          <li role="presentation">
+            <a
+              role="tab"
+              onClick={(e) => (tabs.value = "tab-spec")}
+              aria-selected={tabs.value.endsWith("tab-spec")}
+            >
+              Species
+            </a>
+          </li>
+          <li role="presentation">
+            <a
+              role="tab"
+              onClick={(e) => (tabs.value = "tab-plants")}
+              aria-selected={tabs.value.endsWith("tab-plants")}
+            >
+              Plants
+            </a>
+          </li>
+          <li role="presentation">
+            <a
+              role="tab"
+              onClick={(e) => (tabs.value = "tab-obstacles")}
+              aria-selected={tabs.value.endsWith("tab-obstacles")}
+            >
+              Obstacles
+            </a>
+          </li>
+          <li role="presentation">
+            <a
+              role="tab"
+              onClick={(e) => (tabs.value = "tab-analysis")}
+              aria-selected={tabs.value.endsWith("tab-analysis")}
+            >
+              Analysis
+            </a>
+          </li>
+          {() => "TODO: make analysis a separate panel always visible"}
+        </ul>
 
-			<div role="tabpanel" id="tab-home" aria-selected={tabs.value.endsWith("tab-home")}>
-				<Start  inclStats={true} />&nbsp;<ProgressBar />
-				<ExportImport />
-			</div>
-			<div role="tabpanel" id="tab-spec" aria-selected={tabs.value.endsWith("tab-spec")} style={{
-				width: '100%',
-				height: '100%',
-			}}>
-				<SpeciesList />
-			</div>
-			<div role="tabpanel" id="tab-sim" aria-selected={tabs.value.endsWith("tab-sim")}>
-				<HoursPerTick />
-				<TotalHours />
-				<Randomize />
-				<InitNumber />
-				<Renderer />
-				<SamplesPerPixel />
-				<ExactPreview />
-				<DownloadRoots />
-			</div>
-			<div role="tabpanel" id="tab-plants" aria-selected={tabs.value.endsWith("tab-plants")}>
-				<Seeds />
-			</div>
-			<div role="tabpanel" id="tab-terrain" aria-selected={tabs.value.endsWith("tab-terrain")}>
-				<FieldResolution />
-				<FieldSizeX />
-				<FieldSizeZ />
-				<FieldSizeD />
-				<FieldModelUpload />
-				<FieldItemRegex />
-				<TerrainsList />
-			</div>
-			<div role="tabpanel" id="tab-obstacles" aria-selected={tabs.value.endsWith("tab-obstacles")}>
-				<Obstacles />
-			</div>
-			<div role="tabpanel" id="tab-analysis" aria-selected={tabs.value.endsWith("tab-analysis")}>
-				<Start inclStats={false} />
-				<VisualMapping />
-				<PickedDetails />
-			</div>
-		</nav>
-	</>
-)};
+        <div
+          role="tabpanel"
+          id="tab-home"
+          aria-selected={tabs.value.endsWith("tab-home")}
+        >
+          <Start inclStats={true} />
+          &nbsp;
+          <ProgressBar />
+          <ExportImport />
+        </div>
+        <div
+          role="tabpanel"
+          id="tab-spec"
+          aria-selected={tabs.value.endsWith("tab-spec")}
+          style={{
+            width: "100%",
+            height: "0",
+            flex: 1,
+          }}
+        >
+          <SpeciesList />
+        </div>
+        <div
+          role="tabpanel"
+          id="tab-sim"
+          aria-selected={tabs.value.endsWith("tab-sim")}
+        >
+          <HoursPerTick />
+          <TotalHours />
+          <Randomize />
+          <InitNumber />
+          <Renderer />
+          <SamplesPerPixel />
+          <ExactPreview />
+          <DownloadRoots />
+        </div>
+        <div
+          role="tabpanel"
+          id="tab-plants"
+          aria-selected={tabs.value.endsWith("tab-plants")}
+        >
+          <Seeds />
+        </div>
+        <div
+          role="tabpanel"
+          id="tab-terrain"
+          aria-selected={tabs.value.endsWith("tab-terrain")}
+        >
+          <FieldResolution />
+          <FieldSizeX />
+          <FieldSizeZ />
+          <FieldSizeD />
+          <FieldModelUpload />
+          <FieldItemRegex />
+          <TerrainsList />
+        </div>
+        <div
+          role="tabpanel"
+          id="tab-obstacles"
+          aria-selected={tabs.value.endsWith("tab-obstacles")}
+        >
+          <Obstacles />
+        </div>
+        <div
+          role="tabpanel"
+          id="tab-analysis"
+          aria-selected={tabs.value.endsWith("tab-analysis")}
+        >
+          <Start inclStats={false} />
+          <VisualMapping />
+          <PickedDetails />
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default App;
